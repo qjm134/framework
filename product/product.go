f578cd73a5e2ad977d81386d11719e5957ac8a2e
qingjiming <qingjiming@xiaomi.com>
2021-02-02 17:43:33 +0800

    lock

--- product/product.go
+++ product/product.go
@@ -8,7 +8,6 @@ import (
 	"math/rand"
 	"openapi/stores/mysql"
 	"openapi/stores/redis"
-	"sync"
 	"time"
 )
 
@@ -28,13 +27,14 @@ type Product struct {
 type productInfo struct {
 	productId int
 	redisKey  string
-	lock      sync.Mutex
+	lock      RedisLock
 }
 
 func NewProductInfo(id int) *productInfo {
 	product := &productInfo{
 		productId: id,
 		redisKey:  fmt.Sprintf("openapi:product:%d", id),
+		lock:      RedisLock{fmt.Sprintf("openapi:lock:%d", id), 1},
 	}
 
 	return product
@@ -44,27 +44,21 @@ func (pro *productInfo) Get() (string, error) {
 	key := pro.redisKey
 	//读缓存
 	client := redis.GetClient()
-	val, err := client.Get(key).Result() //1.正常值，err == nil; 2.没有key，读到空，redis.Nil; 3.读到默认值，"*"; 4.redis出问题，得到err
+	val, err := client.Get(key).Result() //1.正常值，err == nil; 2.没有key，读到空，err=redis.Nil; 3.读到默认值，"*"; 4.redis出问题，得到err
 	if err != nil {
-		//redis问题
-		if err != redis.Nil {
-			log.Println(err)
-			return "", err
+		//缓存没有，读db
+
+		//避免缓存击穿，并发只有一个去请求db，其余的坐享其成
+		//如果err!=nil，lock会一直重试，达到阻塞的效果
+		//如果redis连不上等错误，err!=nil，后面会去db取数据
+		if err = pro.lock.Lock(); err == nil {
+			defer pro.lock.Unlock()
 		}
 
-		//缓存没有，读db
-		//避免缓存击穿，并发只有一个去请求db，其余的坐享其成
-		pro.lock.Lock()
-		defer pro.lock.Unlock()
 		//如果是第一个拿到锁，再次读缓存，仍然没有，则去db取，然后放到缓存
 		//如果是后面等待的线程拿到锁，第一个释放锁之前，已经将数据数据放到缓存，所以后面的拿到锁的时候，缓存已经有数据了
 		val, err := client.Get(key).Result()
 		if err != nil {
-			if err != redis.Nil {
-				log.Println(err)
-				return "", err
-			}
-
 			product := &Product{Pid: pro.productId}
 			db := mysql.GetDb()
 			has, err := db.Get(product)
@@ -124,6 +118,7 @@ func (pro *productInfo) readAndWrite() error {
 		log.Println(err)
 		return err
 	}
+
 	if has == false {
 		err = errors.New("not found")
 		log.Println(err)
@@ -146,4 +141,41 @@ func (pro *productInfo) readAndWrite() error {
 	}
 
 	return nil
+}
+
+type RedisLock struct {
+	key    string
+	expire int
+}
+
+//获取锁成功，返回nil
+//获取锁失败，一直重试，直到成功，返回nil
+//其他错误，直接返回error
+func (l RedisLock) Lock() error {
+	client := redis.GetClient()
+	for {
+		ok, err := client.SetNX(l.key, 1, time.Duration(l.expire)*time.Second).Result()
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		if ok {
+			return nil
+		}
+
+		time.Sleep(time.Duration(10) * time.Millisecond)
+	}
+}
+
+//重试3次
+//如果最后都失败，只能等超时释放
+func (l RedisLock) Unlock() {
+	client := redis.GetClient()
+	for i := 0; i < 3; i++ {
+		res, err := client.Del(l.key).Result()
+		if err == nil && res > 0 {
+			break
+		}
+	}
 }
