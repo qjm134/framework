284e0e2f62b2b43c4b982a7eb074fb26ec5af019
qingjiming <qingjiming@xiaomi.com>
2021-02-02 16:11:54 +0800

    lock

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"openapi/stores/mysql"
+	"openapi/stores/redis"
+)
+
 func main() {
+	redis.Init()
+	mysql.Init()
 
 }
--- product/product.go
+++ product/product.go
@@ -5,8 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"openapi/stores/mysql"
 	"openapi/stores/redis"
+	"sync"
 	"time"
 )
 
@@ -26,6 +28,7 @@ type Product struct {
 type productInfo struct {
 	productId int
 	redisKey  string
+	lock      sync.Mutex
 }
 
 func NewProductInfo(id int) *productInfo {
@@ -45,39 +48,60 @@ func (pro *productInfo) Get() (string, error) {
 	if err != nil {
 		//redis问题
 		if err != redis.Nil {
+			log.Println(err)
 			return "", err
 		}
 
 		//缓存没有，读db
 		//避免缓存击穿，并发只有一个去请求db，其余的坐享其成
-		product := &Product{Pid: pro.productId}
-		db := mysql.GetDb()
-		has, err := db.Get(product)
+		pro.lock.Lock()
+		defer pro.lock.Unlock()
+		//如果是第一个拿到锁，再次读缓存，仍然没有，则去db取，然后放到缓存
+		//如果是后面等待的线程拿到锁，第一个释放锁之前，已经将数据数据放到缓存，所以后面的拿到锁的时候，缓存已经有数据了
+		val, err := client.Get(key).Result()
 		if err != nil {
-			return "", err
-		}
+			if err != redis.Nil {
+				log.Println(err)
+				return "", err
+			}
 
-		//数据库中也没有，设默认值，下次访问缓存可以读到，防止雪崩，并设过期时间
-		if has == false {
-			if err = client.Set(key, placeHolder, time.Duration(expireNotFound)*time.Second).Err(); err != nil {
+			product := &Product{Pid: pro.productId}
+			db := mysql.GetDb()
+			has, err := db.Get(product)
+			if err != nil {
+				log.Println(err)
+				return "", err
+			}
+
+			//数据库中也没有，设默认值，下次访问缓存可以读到，防止雪崩，并设过期时间
+			if has == false {
+				if err = client.Set(key, placeHolder, time.Duration(expireNotFound)*time.Second).Err(); err != nil {
+					log.Println(err)
+				}
+				return "", errors.New("not found")
+			}
+
+			//db有数据，设置缓存
+			productStr, err := json.Marshal(product)
+			if err != nil {
+				log.Println(err)
+				return "", err
+			}
+			val = string(productStr)
+			rand.Seed(time.Now().Unix())
+			expiration := expire + rand.Int()
+			if err = client.Set(key, val, time.Duration(expiration)*time.Second).Err(); err != nil {
 				log.Println(err)
 			}
-			return "", errors.New("not found")
+			return val, nil
 		}
-
-		//db有数据，设置缓存
-		productStr, _ := json.Marshal(product)
-		val = string(productStr)
-		expiration := time.Duration(expire) * time.Second
-		if err = client.Set(key, val, expiration).Err(); err != nil {
-			log.Println(err)
-		}
-		return val, nil
 	}
 
 	//读到默认值
 	if val == placeHolder {
-		return "", errors.New("not found")
+		err = errors.New("not found")
+		log.Println(err)
+		return "", err
 	}
 
 	//缓存命中
@@ -97,18 +121,27 @@ func (pro *productInfo) readAndWrite() error {
 	db := mysql.GetDb()
 	has, err := db.Get(product)
 	if err != nil {
+		log.Println(err)
 		return err
 	}
 	if has == false {
-		return errors.New("not found")
+		err = errors.New("not found")
+		log.Println(err)
+		return err
 	}
 
-	productStr, _ := json.Marshal(product)
+	productStr, err := json.Marshal(product)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 	val := string(productStr)
 	key := pro.redisKey
-	expiration := time.Duration(expire) * time.Second
+	rand.Seed(time.Now().Unix())
+	expiration := expire + rand.Int()
 	client := redis.GetClient()
-	if err = client.Set(key, val, expiration).Err(); err != nil {
+	if err = client.Set(key, val, time.Duration(expiration)*time.Second).Err(); err != nil {
+		log.Println(err)
 		return err
 	}
 
